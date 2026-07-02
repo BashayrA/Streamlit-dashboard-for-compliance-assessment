4ac38d5047dd688a4cdbd5c2f84b3818a1f7e7c0
Bashayr.A <162049965+BashayrA@users.noreply.github.com>
2026-07-02 23:21:22 +0300

    Change page title to 'Compliance Assessment Dashboard'

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ import streamlit as st
 # --------------------------------------------------------------------------------------
 
 st.set_page_config(
-    page_title="قطاع الشركات",
+    page_title="Compliance Assessment Dashboard",
     page_icon="📊",
     layout="wide",
     initial_sidebar_state="collapsed",
